b088a23d1ceac6de468168284416329aca3986d8
gokulgoky1 <gokulgr555@gmail.com>
2022-07-10 01:35:33 +0530

    part 2 completed(Home+About)

---
+++ routes/web/home.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    console.log("hello I'm on the start");
+    res.render("home/index");
+ });
+ 
+   router.get("/home",function(req,res){
+     res.render("home/home");
+ })
+
+ router.get("/about",function(req,res){
+    res.render("home/about");
+})
+
+ 
+ module.exports = router; 
